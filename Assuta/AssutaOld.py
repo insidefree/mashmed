8ab8db9e02e72d71c5b90aea878c4f074729708f
Denis Sorokin <denys-s@yit.co.il>
2017-04-04 12:47:32 +0300

    Got doctors list from old assuta

--- Assuta/AssutaOld.py
+++ Assuta/AssutaOld.py
@@ -10,10 +10,6 @@ class AssutaOld(Assuta):
         self.driver.get('http://assuta-hospital.com/nashi-vrachi.aspx')
         links2 = [link.get_attribute('href') for link in
                   self.driver.find_elements_by_xpath("//ul[@class='doctorSlider_list js-doctorSlider_list']/li/a")]
-        ent_list = []
-        ophthalmologist_list = []
-        surgeon_list = []
-        uro_gynecology_list = []
         ent_flag = True
         ophthalmologist_flag = True
         surgeon_flag = True
@@ -28,36 +24,31 @@ class AssutaOld(Assuta):
             if name.text == 'ЛОР-СПЕЦИАЛИСТЫ' and ent_flag is True:
                 ent_list = [link.text for link in self.driver.find_elements_by_xpath(
                     '//*[@id="dBody"]/center/table/tbody/tr[3]/td/div/table/tbody/tr/td[2]/div[2]/div/div[1]/p')]
+                AssutaOld.save_doctors_from_sublist(ent_list[1::])
                 ent_flag = False
 
             if name.text == 'НАШИ ОФТАЛЬМОЛОГИ' and ophthalmologist_flag is True:
                 ophthalmologist_list = [link.text for link in self.driver.find_elements_by_xpath(
                     '//*[@id="dBody"]/center/table/tbody/tr[3]/td/div/table/tbody/tr/td[2]/div[2]/div/div[1]/div[2]/p')]
+                AssutaOld.save_doctors_from_sublist(ophthalmologist_list)
                 ophthalmologist_flag = False
 
             if name.text == 'ПЛАСТИЧЕСКИЕ ХИРУРГИ' and surgeon_flag is True:
                 surgeon_list = [link.text for link in self.driver.find_elements_by_xpath(
-                    '//*[@id="dBody"]/center/table/tbody/tr[3]/td/div/table/tbody/tr/td[2]/div[2]/div/div[1]/div[2]')]
+                    '//*[@id="dBody"]/center/table/tbody/tr[3]/td/div/table/tbody/tr/td[2]/div[2]/div/div[1]/div[2]/p')]
+                AssutaOld.save_doctors_from_sublist(AssutaOld.handling_surgeon_list(surgeon_list, 'Подробнее >>'))
                 surgeon_flag = False
 
             if name.text == 'УРОГИНЕКОЛОГИ' and uro_gynecology_flag is True:
                 uro_gynecology_list = [link.text for link in self.driver.find_elements_by_xpath(
                     '//*[@id="dBody"]/center/table/tbody/tr[3]/td/div/table/tbody/tr/td[2]/div[2]/div/div[1]/p')]
+                AssutaOld.save_doctors_from_sublist(uro_gynecology_list[1::])
                 uro_gynecology_flag = False
-            # doctor = Doctor(name=name.text, birthday='', vacation='', image='', language='', info='', department='',
-            #                 subDepartment='', visible_tg='', status='', first_tg='', link='')
-            # doctor.save()
-        # AssutaOld.save_doctors_from_sublist(ent_list[1::])
-        # AssutaOld.save_doctors_from_sublist(ophthalmologist_list)
-        # AssutaOld.save_doctors_from_sublist([el for el in surgeon_list if el.text == 'Подробнее >>'])
-        tmp = [surgeon_list.index(el) for el in surgeon_list if el == 'Подробнее >>']
-
-        tmp2 = [el for el in surgeon_list[::-1] if surgeon_list.index(el) == tmp[::-1]]
-        for el in tmp2:
-            print(el)
-        # for el in surgeon_list:
-        #     print(el)
-        # AssutaOld.save_doctors_from_sublist(uro_gynecology_list[1::])
+            if name.text != 'ЛОР-СПЕЦИАЛИСТЫ' and name.text != 'НАШИ ОФТАЛЬМОЛОГИ' \
+                    and name.text != 'ПЛАСТИЧЕСКИЕ ХИРУРГИ' and name.text != 'УРОГИНЕКОЛОГИ':
+                doctor = Doctor(name=name.text, birthday='', vacation='', image='', language='', info='', department='',
+                                subDepartment='', visible_tg='', status='', first_tg='', link='')
+            doctor.save()
 
     @staticmethod
     def save_doctors_from_sublist(doctors_list):
@@ -67,7 +58,11 @@ class AssutaOld(Assuta):
                             department='', subDepartment='', visible_tg='', status='', first_tg='', link='')
             doctor.save()
 
-            # def print_doctors_info(self):
-            #     for el in self.get_doctors_links():
-            #         print(el.get_attribute('href'))
-            #     self.driver.quit()
+    @staticmethod
+    def handling_surgeon_list(lst, value):
+        index = [ind for ind, el in enumerate(lst) if el == value]
+        for ind in index[::-1]:
+            del lst[ind]
+            del lst[ind - 1]
+            del lst[ind - 2]
+        return lst
--- test.py
+++ test.py
@@ -1,10 +1,41 @@
-a = [1, 2, 3, 4, 5, 6, 7]
-ia = iter(a)
+a = [10, 2, 3, 4, 5, 6, 3]
+
+# a = a[2:1:-1]
+# for i in a[::-1]:
+#     if i == 3:
+#         # del a[a.index(i)]
+#         a = a[a[a.index(i + 1)]:a[a.index(i)]:-1]
 
 
-for n in a[::2]:
-    index = a.index(n)
-    a[index] *= 2
+# def foo(lst):
+#     index = [ind for ind, x in enumerate(lst) if x == 3]
+#     for ind in index[::-1]:
+#         del lst[ind]
+#         del lst[ind-1]
+#         del lst[ind-2]
+#     return lst
+#
+#
+# for i in foo(a):
+#     print(i)
+#
+# for i in a:
+#     print(i)
+# tmp = [enumerate(a)]
+#
+# for ind, val in tmp:
+#     print(tmp[ind])
 
-for n in a:
-    print(n)
+# for i, val in enumerate(a[::-1]):
+#     tmp = a
+#     print(a[i])
+    # if a[i] == 3:
+    #     del a[i]
+
+# for i in a:
+#     print(i)
+
+for ind, val in enumerate(a):
+    print(f'{ind}:{val}')
+
+# tmp = [ind, val for ind, val in enumerate(a)]
